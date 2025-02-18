f81a61bbd4fed6a8f583906774a3a31df2635d5e
Rafael Mesa <rafaam.2002@gmail.com>
2025-02-18 17:27:16 +0100

    assets created

--- src/factories/ProductFactory.ts
+++ src/factories/ProductFactory.ts
@@ -3,17 +3,22 @@ import { faker } from "@faker-js/faker";
 import { Product } from "../entities/Product";
 
 export class ProductFactory extends Factory<Product> {
-    model = Product;
+  model = Product;
 
-    definition(): Partial<Product> {
+  definition(): Partial<Product> {
+    const productPictures = ["bucal1", "bucal2", "bucal3"];
     return {
       name: faker.person.firstName(),
       description:
         "El producto se deberá adquirir en el establecimiento físico",
       price: faker.number.float({ min: 1, max: 300, fractionDigits: 2 }),
       pictures: faker.datatype.boolean()
-        ? Array.from({ length: faker.number.int({ min: 1, max: 10 }) }, () =>
-            faker.image.avatar()
+        ? Array.from(
+            { length: faker.number.int({ min: 1, max: 10 }) },
+            () =>
+              `/assets/mock/${
+                productPictures[faker.number.int({ min: 0, max: 2 })]
+              }`
           )
         : null,
     };
